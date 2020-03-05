5bbdcdd46760804fcf37dc081a15da00bd3a3164
Matthias Gerstner <matthias.gerstner@suse.de>
2020-03-05 11:55:47 +0100

    verify.py: catch duplicate dictionary entries in JSON

--- verify.py
+++ verify.py
@@ -3,6 +3,13 @@
 import os, sys
 import subprocess
 import glob
+import json
+
+def printerr(*args, **kwargs):
+
+	kwargs["file"] = sys.stderr
+
+	print(*args, **kwargs)
 
 def checkJSONFile(path):
 
@@ -27,8 +34,45 @@ def checkJSONFile(path):
 
 		except FileNotFoundError:
 
-			print("Couldn't find json_verify. You need to install yajl (yet-another-json-lib).", file = sys.stderr)
+			printerr("Couldn't find json_verify. You need to install yajl (yet-another-json-lib).")
 			sys.exit(2)
+
+def checkDuplicateEntries(path):
+
+	# the JSON spec does allow for duplicate dictionary key entries to
+	# appear. And the python json parser acts in some (undefined) way in
+	# this situation, no error is raised.
+	#
+	# therefore hook into the parser to detect duplicate keys and error
+	# out in this case
+
+	errors = []
+
+	def check_duplicates(ordered_pairs):
+
+		d = {}
+
+		for k, v in ordered_pairs:
+			if k in d:
+				# don't raise an exception right away we want
+				# to collect all duplicates not just the first
+				# one
+				errors.append(
+					"duplicate dictionary key in {} encountered: {}".format(path, k)
+				)
+			else:
+				d[k] = v
+
+		return d
+
+	with open(path, 'rb') as json_fd:
+
+		data = json.load(json_fd, object_pairs_hook = check_duplicates)
+
+	for error in errors:
+		printerr(error)
+
+	return len(errors) == 0
 
 our_dir = os.path.dirname(os.path.realpath(__file__))
 pathspec = os.path.join( our_dir, "*.json" )
@@ -37,6 +81,9 @@ res = 0
 for json_file in glob.glob(pathspec):
 
 	if not checkJSONFile(json_file):
+		res = 1
+
+	if not checkDuplicateEntries(json_file):
 		res = 1
 
 sys.exit(res)
